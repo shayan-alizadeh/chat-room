1a15a45fd9d9091e9e467f6731d930bc12f19c05
shyn <shynaz87@gmail.com>
2026-07-12 07:56:12 -0700

    feat : add body parser to app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-import namespaceRoutes from './routes/namespaceRoute.js'
+import namespaceRoutes from "./routes/namespaceRoute.js";
 
-import express from 'express'
+import express from "express";
 
 const app = express();
 
-app.use("/api/v1/namespace",namespaceRoutes)
+//* Body Parser
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+
+//* Cors Policy
+
+//* Template Engine
+
+//*Routes
+app.use("/api/v1/namespace", namespaceRoutes);
 
 export default app;
