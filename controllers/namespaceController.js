2a09616e029668aff2e6a4d00b10c5137276c4fd
shyn <shynaz87@gmail.com>
2026-07-12 07:34:11 -0700

    feat : add getall and create method to namespace controller

--- controllers/namespaceController.js
+++ controllers/namespaceController.js
@@ -1,4 +1,26 @@
+import { namespaceModel } from "../models/chatModel";
 export default class NamespaceController {
-  async getAll(req, res, next) {}
-  async create(req, res, next) {}
+  async getAll(req, res, next) {
+    try {
+      const namespace = namespaceModel.find({}, { room: 0 });
+      return res.status(200).json(namespace);
+    } catch (err) {
+      next(err);
+    }
+  }
+  async create(req, res, next) {
+    try {
+      const { title, href } = req.body;
+      const namespace = namespaceModel.findOne({ $or: [{ title }, { href }] });
+
+      if (namespace)
+        return res.status(400).json({ message: "There is Namespace with this info !!" });
+
+      await namespaceModel.create({ title, href });
+
+      return res.status(201).json({ message: "New namespace created successfully" });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
