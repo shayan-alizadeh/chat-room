9b2bbd7034601ffe0c37ed1a8f83744ea0a82fa5
shyn <shynaz87@gmail.com>
2026-07-12 05:18:42 -0700

    feat : add namespace controller

---
+++ controllers/namespaceController.js
@@ -0,0 +1,4 @@
+export default class NamespaceController {
+  async getAll(req, res, next) {}
+  async create(req, res, next) {}
+}
--- routes/namespaceRoute.js
+++ routes/namespaceRoute.js
@@ -1,5 +1,9 @@
 import express from "express";
+import NamespaceController from "../controllers/namespaceController";
 
 const router = express.Router();
+
+router.get('/',NamespaceController.getAll)
+router.post('/',NamespaceController.create)
 
 export default router;
