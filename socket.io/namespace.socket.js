c78e100718e7905e787de8bb8aefba6e4dd14f11
shyn <shynaz87@gmail.com>
2026-07-18 03:52:18 -0700

    feat : add joining room method

--- socket.io/namespace.socket.js
+++ socket.io/namespace.socket.js
@@ -2,8 +2,7 @@ import { NamespaceModel } from "../models/chatModel";
 
 export function initConnection(io) {
   io.on("connection", async (socket) => {
-
-    socket.broadcast.emit("newMember","New member joined to room .")
+    socket.broadcast.emit("newMember", "New member joined to room .");
 
     const namespaces = await NamespaceModel.find({}).sort({ _id: -1 });
     socket.emit("namespaces", namespaces);
@@ -11,11 +10,19 @@ export function initConnection(io) {
 }
 
 export async function getNamespaceRoom(io) {
-    
   const namespaces = await NamespaceModel.find({}).lean();
 
   namespaces.forEach((namespace) => {
     io.of(namespace.href).on("connection", (socket) => {
+      socket.on("joining", async (newRoom) => {
+        socket.join(newRoom);
+
+        const roomInfo = namespace.rooms.find((room) => {
+          room.title === newRoom;
+        });
+        socket.emit("roomInfo", roomInfo);
+      });
+
       socket.emit("namespaceRoom", namespace.rooms);
     });
   });
